3a8a09ab4b925db7ed9af5a2965645b3516c9d1c
Alessandro Defendenti <alessandrodefendenti@gmail.com>
2018-07-31 23:49:14 +0200

    added huge PRedosky face ^^

--- app/src/main/java/com/rollercoders/predoskysmsbombing/MainActivity.java
+++ app/src/main/java/com/rollercoders/predoskysmsbombing/MainActivity.java
@@ -2,12 +2,22 @@ package com.rollercoders.predoskysmsbombing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button go;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /*
+    TODO: IMPLEMENTARE METODO PER FARE IL BOMBING VERO E PROPRIO
+    TODO: CAPIRE COME CHIAMARE IL SERVIZIO SMS DI ANDROID
+    TODO: SPAMMARE PREDO COME SE NON CI FOSSE 1 DOMANI
+    */
+
 }
